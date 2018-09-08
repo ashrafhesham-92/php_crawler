931fd11ac2edf2cc98b988403a9621e21f7b11a5
ashrafhesham-92 <ashraf_treka_6000@hotmail.com>
2018-09-09 00:43:24 +0200

    making the function call that build initial levels only is the default in crawler_example.php

--- crawler_example.php
+++ crawler_example.php
@@ -23,8 +23,8 @@ try
 
     $crawler = new Crawler($base_url);
 
-    $result = $crawler->crawl($initial_page, 1); // this takes to much time for building the three tree levels of links 0 , 1 and 2
-//    $result = $crawler->crawl($initial_page); // only build tree with initial levels 0 and 1
+//    $result = $crawler->crawl($initial_page, 1); // this takes to much time for building the three tree levels of links 0 , 1 and 2
+    $result = $crawler->crawl($initial_page); // only build tree with initial levels 0 and 1
 
     echo json_encode($result); // prints the crawled links tree as json string so it can be viewed in a JSON viewer
 }
